be2c82c18b3b415438a16c52754a85cdbfe1e657
flyxlion060213-hehe <flyxlion060213@gmail.com>
2025-11-03 13:53:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,21 @@ def home():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
-from flask import send_from_directory
+from flask import Flask, send_from_directory
 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# phục vụ file nhạc ở cùng cấp app.py
 @app.route('/HonLaBan.mp3')
-def serve_music():
-    return send_from_directory('vuichoivahoctap', 'HonLaBan.mp3')
+def music():
+    return send_from_directory('.', 'HonLaBan.mp3')  # dấu '.' nghĩa là thư mục hiện tại
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
 
 
