0625c48222b6eafe3194939f2c48bcac25062cdb
flyxlion060213-hehe <flyxlion060213@gmail.com>
2025-11-05 20:04:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,11 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/HonLaBan.mp3')
-def serve_music():
-    return send_from_directory(os.path.dirname(os.path.abspath(__file__)), 'HonLaBan.mp3')
+@app.route('/music/<filename>')
+def serve_music(filename):
+    return send_from_directory(os.path.dirname(os.path.abspath(__file__)), filename)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
+
+
