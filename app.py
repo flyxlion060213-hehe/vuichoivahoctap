6d4a963ef8b623be1404303e81eee791b7ef84ee
Nguyen Thanh Cong <flyxlion060213@gmail.com>
2025-11-28 15:53:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,12 +7,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/Nhac_nen.mp3')
+@app.route('/Landing.mp3')
 def serve_music():
-    return send_from_directory(os.path.dirname(os.path.abspath(__file__)), 'Nhac_nen.mp3')
+    return send_from_directory(os.path.dirname(os.path.abspath(__file__)), 'Landing.mp3')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
+
 
 
 
